70aba1d597a4709abf31604b81d117ab5cab5dcc
Ryan Peterson <petersonryan97@gmail.com>
2019-07-26 17:06:08 -0700

    Added windowSoftManager.prototype.MODES as constants for easier adjustment

--- www/windowSoftManager.js
+++ www/windowSoftManager.js
@@ -11,7 +11,13 @@ windowSoftManager.prototype.setMode = function(aString){
   function(result){
     /*alert("Error" + reply);*/
    },"windowSoftManager",aString,[]);
-}
+};
+
+windowSoftManager.prototype.MODES = {
+    SOFT_INPUT_ADJUST_PAN: "adjustPan",
+    SOFT_INPUT_ADJUST_RESIZE: "adjustResize",
+    SOFT_INPUT_ADJUST_NOTHING: "adjustNothing"
+};
 
  var windowSoftManager = new windowSoftManager();
  module.exports = windowSoftManager;
